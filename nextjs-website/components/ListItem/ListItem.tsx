244d5cb331e312c7135e3c0aefc385976223e1e6
Or <2694823+ivorco@users.noreply.github.com>
2020-04-16 19:08:17 +0300

    #35 Add blog posts #tags
    Showing tags JSON

--- nextjs-website/components/ListItem/ListItem.tsx
+++ nextjs-website/components/ListItem/ListItem.tsx
@@ -8,12 +8,18 @@ type Props = {
 }
 
 const ListItem: React.FunctionComponent<Props> = ({ data }) => (
+      <p>
   <div className={styles.item}>
     <Link href="/posts/[id]" as={`/posts/${data.sys.id}`}>
       <a><h3>{data.fields.title}</h3></a>
     </Link>
     <span>{new Date(data.sys.createdAt).toLocaleDateString()}</span>
     <p>{(data.fields.excerpt && data.fields.excerpt.trim()) ? data.fields.excerpt : data.fields.postText.split('\n')[0]}</p>
+    <p>
+        {
+          JSON.stringify(data.fields.tags) //.filter((t: null) => t !== undefined).map((tag: { tagName: string; }) => ("#" + tag.tagName + " "))
+        }
+    </p>
   </div>
 )
 
