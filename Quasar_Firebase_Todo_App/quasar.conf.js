dfaaf3d6debf849169ef122e06172c20973f3465
Operel <oriperelman@gmail.com>
2020-04-23 19:30:29 +0300

    Add ts config for quasar

--- Quasar_Firebase_Todo_App/quasar.conf.js
+++ Quasar_Firebase_Todo_App/quasar.conf.js
@@ -10,6 +10,7 @@
 
 module.exports = function (/* ctx */) {
   return {
+    supportTS: true,
     // app boot file (/src/boot)
     // --> boot files are part of "main.js"
     // https://quasar.dev/quasar-cli/cli-documentation/boot-files
