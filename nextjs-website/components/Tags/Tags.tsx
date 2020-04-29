3f51e1cbf59e5128be2fa6acf29da9e4deb7f3d8
Operel <oriperelman@gmail.com>
2020-04-29 09:49:08 +0300

    Fix React key error  for listed elemnts

--- nextjs-website/components/Tags/Tags.tsx
+++ nextjs-website/components/Tags/Tags.tsx
@@ -5,6 +5,6 @@ import styles from './Tags.module.css';
 export default ({ tags }: any) => (
   <p>
     <span>Tags:</span>
-    {tags?.length && tags.map((tag: any) => <span className={styles.tag}>#{tag.name}</span>)}
+    {tags?.length && tags.map((tag: any) => <span key={tag.sys.id} className={styles.tag}>#{tag.name}</span>)}
   </p>
 )
