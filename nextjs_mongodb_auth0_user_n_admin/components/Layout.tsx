42aff76a6fb2cbd251223a3a2a71eb9b74d89c49
Operel <oriperelman@gmail.com>
2020-05-11 22:27:46 +0300

    Layout and global styles

--- nextjs_mongodb_auth0_user_n_admin/components/Layout.tsx
+++ nextjs_mongodb_auth0_user_n_admin/components/Layout.tsx
@@ -15,7 +15,7 @@ const Layout: React.FunctionComponent<Props> = ({
   const { user, loading } = useUser();
   const admin: boolean = user ? user['http://localhost:3000/roles'].includes('Admin') : false;
   return (
-    <>
+    <div className="container">
       <Head>
         <title>{title}</title>
         <meta charSet="utf-8" />
@@ -44,12 +44,46 @@ const Layout: React.FunctionComponent<Props> = ({
           {admin && <Link href="/admin"><a>Admin</a></Link>}
         </nav>
       </header>
-      {children}
+      <main>{children}</main>
       <footer>
         <hr />
         <span>I'm here to stay (Footer)</span>
       </footer>
-    </>
+
+      <style jsx>{`
+        header, footer {
+          height: 45px;
+          width: 100vw;
+          left: 0;
+        }
+
+        header {
+          position: fixed;
+          top: 0;
+          border-bottom: 0.5px solid;
+
+          display: fles;
+          align-items: center;
+        }
+
+        footer {
+          position: absolute;
+          bottom: 0;
+        }
+
+        .container {
+          position: relative;
+          min-height: 100vh;
+        }
+
+        main:before,
+        main:after {
+          content: '';
+          height: 45px;
+          display: block;
+        }
+      `}</style>
+    </div>
   )
 }
 
--- nextjs_mongodb_auth0_user_n_admin/pages/_app.tsx
+++ nextjs_mongodb_auth0_user_n_admin/pages/_app.tsx
@@ -8,6 +8,14 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <UserProvider value={{ user, loading }}>
       <Component {...pageProps} />
+
+      <style global jsx>{`
+        body {
+          margin: 0;
+          background-color: #f0f8ff;
+        }
+      `}</style>
+
     </UserProvider>
     
   )
