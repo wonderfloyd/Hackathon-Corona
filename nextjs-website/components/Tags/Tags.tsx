1b6afdbedd95582b480cd031c8a70fd4785f2d53
Or <2694823+ivorco@users.noreply.github.com>
2020-05-20 00:07:34 +0300

    Minor fix to tags

--- nextjs-website/components/Tags/Tags.tsx
+++ nextjs-website/components/Tags/Tags.tsx
@@ -6,6 +6,6 @@ import Link from 'next/link';
 export default ({ tags }: any) => (
   <p>
     <span>{tags?.length ? "Tags:" : ""}</span>
-    {tags?.length && tags.map((tag: any) => <Link href="/tags/[id]" as={`/tags/${tag.sys.id}`}><span key={tag.sys.id} className={styles.tag}>#{tag.name}</span></Link>)}
+    {tags?.length && tags.map((tag: any) => <Link key={tag.sys.id} href="/tags/[id]" as={`/tags/${tag.sys.id}`}><span key={tag.sys.id} className={styles.tag}>#{tag.name}</span></Link>)}
   </p>
 )
